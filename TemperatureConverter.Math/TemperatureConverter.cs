d1999f335a02f3adf6db368ab0e7b935ec0b0127
Dima <DTashkinov@gmail.com>
2022-11-26 09:58:29 +0000

    Add project files.

---
+++ TemperatureConverter.Math/TemperatureConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemperatureConverter.Math
+{
+	public class TemperatureConverter : ITemperatureConverter
+	{
+		public decimal CelciusToFahrenheit(decimal value)
+		{
+			return value;
+		}
+
+		public decimal FahrenheitToCelcius(decimal value)
+		{
+			return value;
+		}
+	}
+}
---
+++ TemperatureConverter.UI/Mvvm/ViewModelBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemperatureConverter.UI.Mvvm
+{
+	public abstract class ViewModelBase : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected bool SetProperty<T>(ref T field, T value, [CallerMemberName] string propertyName = "")
+		{
+			if (EqualityComparer<T>.Default.Equals(field, value))
+			{
+				return false;
+			}
+
+			field = value;
+
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+			return true;
+		}
+
+		protected void RaisePropertyChanged([CallerMemberName] string propertyName = "")
+			=> PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+	}
+}
